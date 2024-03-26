08c6d3d0009c4ce4164397724532a5615c6937ca
SubarYan <ayanchin85@gmail.com>
2024-03-26 22:08:00 +0100

    new response type has been added

---
+++ src/main/java/com/cdek/java/model/deliverypoint/response/WidgetDeliveryPoint.java
@@ -0,0 +1,17 @@
+package com.cdek.java.model.deliverypoint.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class WidgetDeliveryPoint extends DeliveryPointResponse {
+
+    private List<Double> location;
+
+}
